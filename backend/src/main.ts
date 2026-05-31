adcdc75fb93c3af8cafa0f0e9e00ac3f144c09d7
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-31 12:35:20 +0530

    feat: enhance server setup with CORS support and global API prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: '*',
+  });
+  const configService = app.get(ConfigService);
+  const PORT: number = configService.get('PORT') ?? 3000;
+  app.setGlobalPrefix('/api/v1');
+  await app.listen(PORT);
+  console.log(`Server is running on http://localhost:${PORT}/api/v1`);
 }
 bootstrap();
