d6915bf81abfd7a994bf044ea61ceb3e60bbcc65
Tejas Angadi <tejasangadi456@gmail.com>
2026-06-13 18:22:37 +0530

    refactor: reorganize import statements for improved clarity in app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerModule } from 'nestjs-pino';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import getDatabaseConfig from './config/database.config';
+import loggerConfig from './config/logger.config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { ActivitiesModule } from './activities/activities.module';
 
 @Module({
   imports: [
@@ -12,6 +18,11 @@ import getDatabaseConfig from './config/database.config';
       inject: [ConfigService],
       useFactory: getDatabaseConfig,
     }),
+    LoggerModule.forRoot(loggerConfig),
+    AuthModule,
+    UsersModule,
+    RoomsModule,
+    ActivitiesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
